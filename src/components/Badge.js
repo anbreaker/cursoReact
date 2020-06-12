5df3ce479c8fa198a1f24b71a6b68079d2b6b008
anbreaker <antunez19@gmail.com>
2020-06-12 20:25:19 +0200

    work with css

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className='Badge'>
+        <div className='Badge__header'>
           <img src={confLogo} alt='Logo de la conferencia' />
         </div>
 
-        <div>
+        <div className='Badge__section-name'>
           <img
+            className='Badge__avatar'
             src='https://s.gravatar.com/avatar/315358dc3891fc64420dd02b9faf0287?s=80'
             alt='Avatar'
           />
           <h1>
-            anbreaker <br /> development
+            anbreaker <br /> <a href='https://rootdevs.es/'>rootdevs.es</a>
           </h1>
         </div>
 
-        <div>
-          <p>Student Frontend</p>
-          <p>@anbreaker</p>
+        <div className='Badge__section-info'>
+          <h3>Student Frontend</h3>
+          <div>@anbreaker</div>
         </div>
-        <div>#platziConf</div>
+        <div className='Badge__footer'>#platziConf</div>
       </div>
     );
   }
