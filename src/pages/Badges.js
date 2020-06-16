0d0b7ec33168f771c7e92879c9256b9f1618a5c7
anbreaker <antunez19@gmail.com>
2020-06-16 08:45:43 +0200

    data real time

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -6,6 +6,7 @@ import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 import PageLoading from '../components/PageLoading';
 import PageError from '../components/PageError';
+import MiniLoader from '../components/MiniLoader';
 import api from '../api';
 
 class Badges extends React.Component {
@@ -22,6 +23,12 @@ class Badges extends React.Component {
 
   componentDidMount() {
     this.fetchData();
+
+    this.intervalId = setInterval(this.fetchData, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   fetchData = async () => {
@@ -37,7 +44,7 @@ class Badges extends React.Component {
   };
 
   render() {
-    if (this.state.loading === true) {
+    if (this.state.loading === true && !this.state.data) {
       return <PageLoading />;
     }
 
@@ -67,6 +74,8 @@ class Badges extends React.Component {
         <div className='Badges__list'>
           <div className='Badges__container'>
             <BadgesList badges={this.state.data} />
+
+            {this.state.loading && <MiniLoader />}
           </div>
         </div>
       </React.Fragment>
