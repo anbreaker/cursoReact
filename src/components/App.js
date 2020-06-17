fec7966b890e68f010cb2ad080262eae630f482e
anbreaker <antunez19@gmail.com>
2020-06-17 08:33:10 +0200

    advance in crud

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Layout from './Layout';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
-import BadgeEdit from '../pages/BadgeEdit';
+// import BadgeEdit from '../pages/BadgeEdit';
+import BadgeDetails from '../pages/BadgeDetails';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
 
@@ -16,7 +17,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/badges' component={Badges} />
           <Route exact path='/badges/new' component={BadgeNew} />
-          <Route exact path='/badges/:badgeId/edit' component={BadgeEdit} />
+          <Route exact path='/badges/:badgeId/' component={BadgeDetails} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
